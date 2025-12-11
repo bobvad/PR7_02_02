ae92e6934e8c3c96dd1de74397e9aad05c367862
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-12-11 12:32:16 +0500

    Получение информации с сайта. Дегтянников.

--- HttpNewsPAT/Program.cs
+++ HttpNewsPAT/Program.cs
@@ -8,7 +8,21 @@ namespace HttpNewsPAT
     {
         static void Main(string[] args)
         {
-           
+            WebRequest Request = WebRequest.Create("http://news.permaviat.ru/main.php");
+            using (HttpWebResponse Response = (HttpWebResponse)Request.GetResponse())
+            {
+                Console.WriteLine(Response.StatusDescription);
+                using (Stream DataStream = Response.GetResponseStream())
+                {
+                    using (StreamReader Reader = new StreamReader(DataStream))
+                    {
+                        string ResponseFromServer = Reader.ReadToEnd();
+                        Console.WriteLine(ResponseFromServer);
+                    }
+                }
+            }
+            Console.Read();
         }
+
     }
 }
