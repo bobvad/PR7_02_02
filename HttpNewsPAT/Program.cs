729e6e4cea2baf67e6d780ed02fd9ca88f839d8e
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-12-11 10:50:27 +0500

    Добавьте файлы проекта.

---
+++ HttpNewsPAT/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace HttpNewsPAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
